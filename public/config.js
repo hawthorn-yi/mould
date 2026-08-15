b2878bd3936a0933a3639623c3a8e50fa6ff92f1
hawthorn-yi <13240590212@163.com>
2026-08-15 16:23:14 +0800

    Add public/config.js

---
+++ public/config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: 'https://jvojnntbpephgblrlhpb.supabase.co',
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2b2pubnRicGVwaGdibHJsaHBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3NzY4MTIsImV4cCI6MjEwMjM1MjgxMn0.P35Bm6HrmshokkIY6vpBKwC34Yaj635r_NyPZecaGMw',
+};
